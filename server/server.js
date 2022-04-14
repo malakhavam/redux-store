975cbc8ed47a298b06a9b81387218e02f080e70c
malakhavam <malakhava@yahoo.com>
2022-04-14 00:43:58 -0400

    trying to fix prod

--- server/server.js
+++ server/server.js
@@ -30,8 +30,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 }
 
 db.once('open', () => {
